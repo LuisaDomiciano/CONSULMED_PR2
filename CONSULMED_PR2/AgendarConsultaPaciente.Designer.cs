0496bbba4d955ddf2cd9609c3aa2df61ab21736c
LuisaDomiciano <domicianoluisa@gmail.com>
2025-12-02 13:05:23 -0300

    AAA

--- CONSULMED_PR2/AgendarConsultaPaciente.Designer.cs
+++ CONSULMED_PR2/AgendarConsultaPaciente.Designer.cs
@@ -31,6 +31,8 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AgendarConsultaPaciente));
             this.ComboBoxEspecialidadesAgendamento = new System.Windows.Forms.ComboBox();
+            this.medicoProfissionalBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cJ3027392PR2DataSet1 = new CONSULMED_PR2.CJ3027392PR2DataSet1();
             this.ComboBoxMedicosAgendamentos = new System.Windows.Forms.ComboBox();
             this.ComboBoxHorariosAgendamentosPac = new System.Windows.Forms.ComboBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
@@ -40,13 +42,11 @@
             this.cJ3027392PR2DataSet = new CONSULMED_PR2.CJ3027392PR2DataSet();
             this.especialidadesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.especialidadesTableAdapter = new CONSULMED_PR2.CJ3027392PR2DataSetTableAdapters.EspecialidadesTableAdapter();
-            this.cJ3027392PR2DataSet1 = new CONSULMED_PR2.CJ3027392PR2DataSet1();
-            this.medicoProfissionalBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.medico_ProfissionalTableAdapter = new CONSULMED_PR2.CJ3027392PR2DataSet1TableAdapters.Medico_ProfissionalTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.medicoProfissionalBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cJ3027392PR2DataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.cJ3027392PR2DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.especialidadesBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.cJ3027392PR2DataSet1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.medicoProfissionalBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // ComboBoxEspecialidadesAgendamento
@@ -56,12 +56,22 @@
             this.ComboBoxEspecialidadesAgendamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ComboBoxEspecialidadesAgendamento.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.ComboBoxEspecialidadesAgendamento.FormattingEnabled = true;
-            this.ComboBoxEspecialidadesAgendamento.Location = new System.Drawing.Point(299, 171);
+            this.ComboBoxEspecialidadesAgendamento.Location = new System.Drawing.Point(316, 173);
             this.ComboBoxEspecialidadesAgendamento.Name = "ComboBoxEspecialidadesAgendamento";
             this.ComboBoxEspecialidadesAgendamento.Size = new System.Drawing.Size(216, 30);
             this.ComboBoxEspecialidadesAgendamento.TabIndex = 0;
             this.ComboBoxEspecialidadesAgendamento.ValueMember = "Especialidade_Principal";
             this.ComboBoxEspecialidadesAgendamento.SelectedIndexChanged += new System.EventHandler(this.ComboBoxEspecialidadesAgendamento_SelectedIndexChanged);
+            // 
+            // medicoProfissionalBindingSource
+            // 
+            this.medicoProfissionalBindingSource.DataMember = "Medico_Profissional";
+            this.medicoProfissionalBindingSource.DataSource = this.cJ3027392PR2DataSet1;
+            // 
+            // cJ3027392PR2DataSet1
+            // 
+            this.cJ3027392PR2DataSet1.DataSetName = "CJ3027392PR2DataSet1";
+            this.cJ3027392PR2DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // ComboBoxMedicosAgendamentos
             // 
@@ -110,7 +120,7 @@
             this.BtnSetaNextAgendarPac.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnSetaNextAgendarPac.Image = ((System.Drawing.Image)(resources.GetObject("BtnSetaNextAgendarPac.Image")));
             this.BtnSetaNextAgendarPac.Location = new System.Drawing.Point(740, 412);
-            this.BtnSetaNextAgendarPac.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnSetaNextAgendarPac.Margin = new System.Windows.Forms.Padding(2);
             this.BtnSetaNextAgendarPac.Name = "BtnSetaNextAgendarPac";
             this.BtnSetaNextAgendarPac.Size = new System.Drawing.Size(28, 28);
             this.BtnSetaNextAgendarPac.TabIndex = 6;
@@ -120,7 +130,7 @@
             // 
             this.BtnNextAgendarPac.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnNextAgendarPac.Location = new System.Drawing.Point(659, 412);
-            this.BtnNextAgendarPac.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnNextAgendarPac.Margin = new System.Windows.Forms.Padding(2);
             this.BtnNextAgendarPac.Name = "BtnNextAgendarPac";
             this.BtnNextAgendarPac.Size = new System.Drawing.Size(83, 28);
             this.BtnNextAgendarPac.TabIndex = 7;
@@ -142,16 +152,6 @@
             // 
             this.especialidadesTableAdapter.ClearBeforeFill = true;
             // 
-            // cJ3027392PR2DataSet1
-            // 
-            this.cJ3027392PR2DataSet1.DataSetName = "CJ3027392PR2DataSet1";
-            this.cJ3027392PR2DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // medicoProfissionalBindingSource
-            // 
-            this.medicoProfissionalBindingSource.DataMember = "Medico_Profissional";
-            this.medicoProfissionalBindingSource.DataSource = this.cJ3027392PR2DataSet1;
-            // 
             // medico_ProfissionalTableAdapter
             // 
             this.medico_ProfissionalTableAdapter.ClearBeforeFill = true;
@@ -171,10 +171,10 @@
             this.Name = "AgendarConsultaPaciente";
             this.Text = "AgendarConsultaPaciente";
             this.Load += new System.EventHandler(this.AgendarConsultaPaciente_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.medicoProfissionalBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cJ3027392PR2DataSet1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.cJ3027392PR2DataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.especialidadesBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.cJ3027392PR2DataSet1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.medicoProfissionalBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
